e0503691c37fa21ad4709b20a607ba3a6be790a1
Dave Tillman <dtillman@pivotal.io>
2016-02-19 14:04:02 -0700

    Initial checkin of Asp.NET 4.5 sample app for CloudFoundry

---
+++ samples/SimpleCloudFoundry4/SimpleCloudFoundry4/ViewModels/CloudFoundryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Spring.Extensions.Configuration.CloudFoundry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCloudFoundry4.ViewModels
+{
+    public class CloudFoundryViewModel
+    {
+        public CloudFoundryViewModel(CloudFoundryApplicationOptions appOptions, CloudFoundryServicesOptions servOptions)
+        {
+            CloudFoundryServices = servOptions;
+            CloudFoundryApplication = appOptions;
+        }
+        public CloudFoundryServicesOptions CloudFoundryServices { get; }
+        public CloudFoundryApplicationOptions CloudFoundryApplication { get; }
+    }
+}

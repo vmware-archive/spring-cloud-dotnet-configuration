836c3606a56f1689304ed51d6f30274aca78f12f
Dave Tillman <dtillman@pivotal.io>
2016-05-10 16:05:00 -0600

    Fix dependencies script

--- scripts/patch-project-json.js
+++ scripts/patch-project-json.js
@@ -40,8 +40,8 @@ jsonfile.readFile(file, function (err, project) {
     project.version = version;
 
     // Patch dependencies
-    project['SteelToe.Extensions.Configuration.CloudFoundry'] = dependsVersion;
-    project['SteelToe.Extensions.Configuration.ConfigServer'] = dependsVersion;    
+    project.dependencies['SteelToe.Extensions.Configuration.CloudFoundry'] = dependsVersion;
+    project.dependencies['SteelToe.Extensions.Configuration.ConfigServer'] = dependsVersion;    
 
     jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
         if (err)

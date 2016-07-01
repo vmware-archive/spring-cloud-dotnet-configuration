0f42f190d3c0820ccc8fd4620710634a22a2b81b
Dave Tillman <dtillman@pivotal.io>
2016-07-01 11:41:43 -0600

    Fixup travis build

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+export STEELTOE_VERSION="1.0.0"
 export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
 export PATH="$DOTNET_INSTALL_DIR:$PATH"
+# Patch project.json files
+cd ./scripts
+npm install
+node patch-project-json.js ../src/Pivotal.Extensions.Configuration.ConfigServer/project.json $STEELTOE_VERSION-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER $TRAVIS_TAG
+cd ..
 cd src
 dotnet restore
 cd ../test

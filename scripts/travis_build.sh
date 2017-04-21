3f3364ce73142598f207305f69be24d842471dcb
Dave Tillman <dtillman@pivotal.io>
2017-04-21 06:30:52 -0600

    Change to .csproj

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-export STEELTOE_VERSION="1.0.1"
-export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
-export PATH="$DOTNET_INSTALL_DIR:$PATH"
-# Patch project.json files
-cd ./scripts
-npm install
-node patch-project-json.js ../src/Pivotal.Extensions.Configuration.ConfigServer/project.json $STEELTOE_VERSION-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER $TRAVIS_TAG
-cd ..
-cd src
-dotnet restore
-cd ../test
-dotnet restore
-cd ..
+
+echo Code is built in Unit Tests
+
 cd src/Pivotal.Extensions.Configuration.ConfigServer
-dotnet build --framework netstandard1.3 --configuration Release
+dotnet restore --configfile ../../nuget.config
 cd ../..

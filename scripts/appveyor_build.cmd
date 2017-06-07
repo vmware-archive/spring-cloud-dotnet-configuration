c0bbb382ebdd03a1fd2af6249badd746582bda67
Dave Tillman <dtillman@pivotal.io>
2017-06-07 16:51:15 -0600

    Symbols

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -4,6 +4,6 @@
 cd %APPVEYOR_BUILD_FOLDER%
 cd src\Pivotal.Extensions.Configuration.ConfigServer
 dotnet restore --configfile ..\..\nuget.config
-IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE%)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration %BUILD_TYPE% --version-suffix %STEELTOE_VERSION_SUFFIX% --include-symbols --include-source)
 cd %APPVEYOR_BUILD_FOLDER%
